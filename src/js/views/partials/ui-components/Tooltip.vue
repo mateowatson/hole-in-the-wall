af002fb942ad9fe1ec57b99e39e6545c96ccb523
Matt Watson <matt@mattwatson.org>
2020-11-25 20:03:15 -0600

    Improved tooltip

--- src/js/views/partials/ui-components/Tooltip.vue
+++ src/js/views/partials/ui-components/Tooltip.vue
@@ -1,6 +1,6 @@
 <template>
-    <span class="hitw_tooltip rounded d-block position-absolute text-light bg-dark">
-        <span class="position-relative">
+    <span class="hitw_tooltip rounded d-inline-block position-absolute">
+        <span class="position-relative text-light bg-dark small p-1 rounded">
             <slot></slot>
         </span>
     </span>
